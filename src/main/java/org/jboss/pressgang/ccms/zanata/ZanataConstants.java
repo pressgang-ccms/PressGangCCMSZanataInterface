0d17e5fe6ac0e42eccdb4bc55ea73673fa14dd63
Lee Newson <lnewson@redhat.com>
2013-11-19 09:56:58 +1000

    More work on BZ#1013888 - Load application config from a configuration file.

--- src/main/java/org/jboss/pressgang/ccms/zanata/ZanataConstants.java
+++ src/main/java/org/jboss/pressgang/ccms/zanata/ZanataConstants.java
@@ -4,30 +4,30 @@ public final class ZanataConstants {
     /**
      * The system property that identifies the zanata server to send files to for translation
      */
-    public static final String ZANATA_SERVER_PROPERTY = "topicIndex.zanataServer";
+    public static final String ZANATA_SERVER_PROPERTY = "pressgang.zanataServer";
 
     /**
      * The system property that identifies the zanata project name
      */
-    public static final String ZANATA_PROJECT_PROPERTY = "topicIndex.zanataProject";
+    public static final String ZANATA_PROJECT_PROPERTY = "pressgang.zanataProject";
 
     /**
      * The system property that identifies the zanata user name
      */
-    public static final String ZANATA_USERNAME_PROPERTY = "topicIndex.zanataUsername";
+    public static final String ZANATA_USERNAME_PROPERTY = "pressgang.zanataUsername";
 
     /**
      * The system property that identifies the zanata project version
      */
-    public static final String ZANATA_PROJECT_VERSION_PROPERTY = "topicIndex.zanataProjectVersion";
+    public static final String ZANATA_PROJECT_VERSION_PROPERTY = "pressgang.zanataProjectVersion";
 
     /**
      * The system property that identifies the zanata API token
      */
-    public static final String ZANATA_TOKEN_PROPERTY = "topicIndex.zanataToken";
+    public static final String ZANATA_TOKEN_PROPERTY = "pressgang.zanataToken";
     
     /**
      * The system property that identifies the time that should be waited in between each zanata call.
      */
-    public static final String MIN_ZANATA_CALL_INTERNAL_PROPERTY = "topicIndex.minZanataCallInterval";
+    public static final String MIN_ZANATA_CALL_INTERNAL_PROPERTY = "pressgang.minZanataCallInterval";
 }
